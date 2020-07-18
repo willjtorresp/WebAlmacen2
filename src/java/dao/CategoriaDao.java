eb8bd1df50ce409c5411db18d4d3b3954bd53690
willjtorresp <66891038+willjtorresp@users.noreply.github.com>
2020-07-18 10:59:31 -0500

    CategoriaDao
    Se incluye Codigo

--- src/java/dao/CategoriaDao.java
+++ src/java/dao/CategoriaDao.java
@@ -5,10 +5,57 @@
  */
 package dao;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import model.Categoria;
+
 /**
  *
  * @author wtorr_000
  */
 public class CategoriaDao {
+    public static boolean registrar(Categoria cat){
+        try {
+            String SQL="INSERT INTO categoria(categoria) VALUES(?);";
+            Connection con=conexion.conectar();
+            PreparedStatement st=con.prepareStatement(SQL);
+            st.setString(1, cat.getCategoria());
+            if(st.executeUpdate()>0){
+                return true;
+            }else{
+                return false;
+            }
+        } catch (SQLException ex) {
+            return false;
+        }
+        
+    }
+    
+    public static ArrayList<Categoria> listar(){
+        try {
+            String SQL="SELECT * FROM categoria;";
+            Connection con=conexion.conectar();
+            PreparedStatement st=con.prepareStatement(SQL);
+            //st.setString(1, cat.getCategoria());
+            ResultSet resultado=st.executeQuery();
+            ArrayList<Categoria> lista=null;
+            Categoria cat;
+            while(resultado.next()){
+                cat=new Categoria();
+                cat.setId_categoria(resultado.getInt("Id_categoria"));
+                cat.setCategoria(resultado.getString("categoria"));
+                lista.add(cat);                
+            }
+            return lista;
+        } catch (SQLException ex) {
+            return null;
+        }
+        
+    }
     
 }
